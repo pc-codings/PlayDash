6b9ec6fb6d6d6c5b20dd17c4a787286271ad2ad4
Paritosh <paritoshchowdhury6264@gmail.com>
2024-01-19 19:23:56 +0530

    add tokens

--- src/index.js
+++ src/index.js
@@ -8,6 +8,14 @@ dotenv.config ({
 })
 
 connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000, ()=>{
+        console.log(`server is listening on ${process.env.PORT}`)
+    })
+})
+.catch((err) => {
+    console.error(err)
+})
 
 
 
